151caa77d823b7fc1c8dbd331d56e5a32b7f3182
dayne beronque <dayneyulber@gmail.com>
2020-03-30 18:17:26 -0500

    Add Loading Content

--- src/App/index.js
+++ src/App/index.js
@@ -4,13 +4,16 @@ import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider';
 import Setings from '../Settings';
+import Content from '../Shared/Content';
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar />
-        <Setings />
+        <Content>
+          <Setings />
+        </Content>
       </AppProvider>
     </AppLayout>
   );
