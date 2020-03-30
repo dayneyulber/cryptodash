ed4ef8c7fba29b9e9c462f5b0a452996a84ef131
dayne beronque <dayneyulber@gmail.com>
2020-03-30 15:07:54 -0500

    add app bar + layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Welcome from './WelcomeMessage'
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+
 
 function App() {
   return (
+  <AppLayout>
+    <AppBar/>
     <Welcome/>
+  </AppLayout>
   );
 }
 
