e966a0b10f85db38ac631e70cd8c0694fafdd7e9
dayne beronque <dayneyulber@gmail.com>
2020-03-30 16:42:08 -0500

    Settings page, localStorage

---
+++ src/Settings/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Welcome from './WelcomeMessage'
+import ConfirmButton from './ConfirmButton';
+export default function(){
+    return <div> <Welcome/> <ConfirmButton/> </div>
+}
