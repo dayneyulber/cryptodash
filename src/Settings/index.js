e4da367a990ca14501f1bda56806297fe072d9fa
dayne beronque <dayneyulber@gmail.com>
2020-03-31 15:20:59 -0500

    Add Search

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Welcome from './WelcomeMessage'
+import WelcomeMessage from './WelcomeMessage'
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
-
+import Search from './Search';
 export default function(){
     return <Page name = "settings"> 
-        <Welcome/> 
+        <WelcomeMessage/> 
         <CoinGrid topSection/>
         <ConfirmButton/> 
+        <Search/>
         <CoinGrid/>
     </Page>
 }
