c77f45d5cdae4d294908401bfc3c8f0f3ba27513
dayne beronque <dayneyulber@gmail.com>
2020-04-02 16:59:55 -0500

    Add ChartSelect

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -14,6 +14,7 @@ export class AppProvider extends React.Component {
            this.state = {
              page: "dashboard",
              favorites: ["BTC", "ETH", "XMR", "DOGE"],
+             timeInterval: 'months',
              ...this.savedSettings(),
              setPage: this.setPage,
              addCoin: this.addCoin,
@@ -21,7 +22,8 @@ export class AppProvider extends React.Component {
              isInFavorites: this.isInFavorites,
              confirmFavorites: this.confirmFavorites,
              setCurrentFavorite: this.setCurrentFavorite,
-             setFilteredCoins: this.setFilteredCoins
+             setFilteredCoins: this.setFilteredCoins,
+             changeChartSelect: this.changeChartSelect
            };
          }
 
@@ -52,7 +54,7 @@ export class AppProvider extends React.Component {
              {
                name: this.state.currentFavorite,
                data: results.map((ticker, index) => [
-                 moment().subtract({months: TIME_UNITS - index}).valueOf(),
+                 moment().subtract({[this.state.timeInterval]: TIME_UNITS - index}).valueOf(),
                  ticker.USD
                ])
              }
@@ -84,7 +86,7 @@ export class AppProvider extends React.Component {
                  this.state.currentFavorite,
                  ['USD'],
                  moment()
-                   .subtract({months: units})
+                   .subtract({[this.state.timeInterval]: units})
                    .toDate()
                )
              );
@@ -157,6 +159,10 @@ export class AppProvider extends React.Component {
          setPage = page => this.setState({ page });
 
          setFilteredCoins = filteredCoins => this.setState({ filteredCoins });
+
+         changeChartSelect = (value) => {
+          this.setState({timeInterval: value, historical:null}, this.fetchHistorical);
+         }
 
          render() {
            return (
