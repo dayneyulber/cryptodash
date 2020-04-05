50e6783df83e75d167b05b58fb2ca70f34e42682
dayne beronque <dayneyulber@gmail.com>
2020-04-05 10:56:03 -0500

    Updates

---
+++ precache-manifest.c3bb4144cda03bc6c88f2cdedc0c21f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f7cb53f079433613701e12d53d9483",
+    "url": "/cryptodash/index.html"
+  },
+  {
+    "revision": "1ae7cbfe404a8614d992",
+    "url": "/cryptodash/static/css/main.9ac43c5e.chunk.css"
+  },
+  {
+    "revision": "28acbbad97d064dd3c16",
+    "url": "/cryptodash/static/js/2.25ea2ab1.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/cryptodash/static/js/2.25ea2ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae7cbfe404a8614d992",
+    "url": "/cryptodash/static/js/main.97fd819f.chunk.js"
+  },
+  {
+    "revision": "a4557b01c8185771facb",
+    "url": "/cryptodash/static/js/runtime-main.83ad6dfc.js"
+  }
+]);
